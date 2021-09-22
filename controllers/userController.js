e8d0ac8f3ce3a9df665322aae7470dd56d6b3561
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-09-22 00:21:50 -0400

    hitting login endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 let router = require('express').Router()
+const { UniqueConstraintError } = require('sequelize/lib/errors')
 const { User } = require('../models')
 
 router.post('/register', async (req, res) => {
@@ -15,10 +16,43 @@ router.post('/register', async (req, res) => {
          user,
       }
    } catch (err) {
-      console.log(err)
-      msg = {
-         msg: 'Failed to create user.',
+      err instanceof UniqueConstraintError
+         ? res.status(409).json({
+              message: 'Username is already in use.',
+           })
+         : res.status(500).json({
+              msg: 'Failed to create user.',
+              err,
+           })
+   }
+   res.json(message)
+})
+
+router.post('/login', async (req, res) => {
+   let message
+   console.log(User)
+   const { email, password } = req.body
+   try {
+      const user = await User.findOne({
+         where: { email: email, password: password },
+      })
+      if (user) {
+         message = {
+            msg: 'User found.',
+            user,
+         }
+      } else {
+         res.status(404).json({
+            msg: 'User does not exist.',
+            err,
+         })
       }
+   } catch (err) {
+      console.log(err)
+      res.status(500).json({
+         msg: 'Cannot find user.',
+         err,
+      })
    }
    res.json(message)
 })
